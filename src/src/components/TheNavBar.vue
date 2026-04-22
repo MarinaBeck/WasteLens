66d67c57c774203b75a5ff36ec4687a2b345616c
Desiree <desiree.aichstill@hotmail.com>
2026-04-22 15:10:24 +0200

    logo changes

--- src/src/components/TheNavBar.vue
+++ src/src/components/TheNavBar.vue
@@ -1,11 +1,11 @@
+<script setup>
+import logoUrl from '../assets/wastelense-logo-icon-512.svg'
+</script>
+
 <template>
   <nav>
     <a href="#" class="nav-logo">
-      <svg viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg" width="28" height="28">
-        <circle cx="14" cy="14" r="13" fill="oklch(89% 0.055 142)"/>
-        <path d="M14 6C10 6 7 9 7 13c0 3 1.5 5.5 4 6.5V22h6v-2.5c2.5-1 4-3.5 4-6.5 0-4-3-7-7-7z" fill="oklch(31% 0.095 148)"/>
-        <path d="M11 13c0-1.66 1.34-3 3-3s3 1.34 3 3-1.34 3-3 3-3-1.34-3-3z" fill="oklch(89% 0.055 142)"/>
-      </svg>
+      <img :src="logoUrl" alt="WasteLense Logo" class="nav-logo-img" />
       WasteLense
     </a>
     <div class="nav-links">
@@ -28,6 +28,12 @@ nav {
   background: oklch(96.5% 0.012 82 / 0.88);
   backdrop-filter: blur(12px);
   border-bottom: 1px solid var(--border);
+}
+
+.nav-logo-img {
+  width: 32px;
+  height: 32px;
+  object-fit: contain;
 }
 
 .nav-logo {
